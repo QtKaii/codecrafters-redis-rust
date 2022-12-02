3e3efd48012202709eb0d96c2dd6f770155037d0
Kai <21qtkai@gmail.com>
2022-12-02 09:34:17 +0000

    Multi

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,38 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let mut listener = TcpListener::bind("127.0.0.1:6379").await?;
-
-    let ping_response = b"+PONG\r\n";
 
     loop {
         let incoming = listener.accept().await;
         match incoming {
-            ok(mut strem, _) => {
+            Ok((mut stream, _)) => {
+                println!("accepted new connection");
                 tokio::spawn(async move {
                     handle_connection(&mut stream).await.unwrap();
-                })
-            },
+                });
+            }
             Err(e) => {
-                println!("Error: {}", e)
+                println!("error: {}", e);
             }
         }
     }
 }
 
 async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
-    let mut buf = [0;512];
+    let mut buf = [0; 512];
 
     let response = b"+PONG\r\n";
 
     loop {
-        let b_read = stream.read(&mut buf).await?;
-        if b_read == 0 {
-            println!("Client closed the connection");
+        // Wait for the client to send us a message but ignore the content for now
+        let bytes_read = stream.read(&mut buf).await?;
+        if bytes_read == 0 {
+            println!("client closed the connection");
             break;
         }
 
-        stream.write(response).await?
+        stream.write(response).await?;
     }
 
     Ok(())
